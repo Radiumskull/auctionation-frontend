95e6605c7aae7df57ad0aa870a8f8ecc7cf379f4
Aritra Bhattacharjee <whoisaritra@gmail.com>
2021-03-01 20:44:46 +0530

    Risky

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyC68hKyS70mm2whyZafx1m8-zwTyfWX9Xc",
+  authDomain: "auctionation-3d056.firebaseapp.com",
+  databaseURL: "https://auctionation-3d056-default-rtdb.firebaseio.com",
+  projectId: "auctionation-3d056",
+  storageBucket: "auctionation-3d056.appspot.com",
+  messagingSenderId: "132999670882",
+  appId: "1:132999670882:web:727be1bbb721ac74cf353b",
+  measurementId: "G-RPLBKBRSL0"
+};
